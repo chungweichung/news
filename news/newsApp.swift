0b38e73a7b6b36c626dee05d87dcbd9e9df5fe4a
weichung <bio891128@gmail.com>
2022-10-02 18:32:39 +0800

    Initial Commit

---
+++ news/newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsApp.swift
+//  news
+//
+//  Created by Weichung Chung on 2022/10/2.
+//
+
+import SwiftUI
+
+@main
+struct newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
